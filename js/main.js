c2f611951b7155301d5712f0852b99fe5e4f97f6
Kevin Lewis <get@jellybabi.es>
2015-10-15 11:53:18 +0100

    deploy from ship

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+$('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top - 82
+    }, 500, 'swing', function () {
+        window.location.hash = target;
+    });
+});
+
 $("#faq li, #challenges li, #data li, #venue .travel > *").matchHeight();
 
 $("#schedule nav h2").click(function() {
